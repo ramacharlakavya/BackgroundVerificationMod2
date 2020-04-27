a13989855c973a6c4c33b948408b692371f3ce8b
home <home@Nagarjuna>
2020-04-27 11:46:07 +0530

    second commit.

--- VerificationById/src/main/java/com/cg/backgroundverification/VerificationById.java
+++ VerificationById/src/main/java/com/cg/backgroundverification/VerificationById.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FetchEmployeeDocumentByIdServiceApplication {
+public class VerificationById {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FetchEmployeeDocumentByIdServiceApplication.class, args);
+		SpringApplication.run(VerificationById.class, args);
 	}
 
 }
